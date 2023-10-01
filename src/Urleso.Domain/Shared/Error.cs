55622238940a7aa06166b69116628bc1c9f3e8cd
exqzmepls <exqzmepls@yandex.ru>
2023-10-01 15:45:12 +0500

    Add result objects

---
+++ src/Urleso.Domain/Shared/Error.cs
@@ -0,0 +1,14 @@
+﻿namespace Urleso.Domain.Shared;
+
+public sealed class Error
+{
+    internal Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+
+    public string Message { get; }
+}
