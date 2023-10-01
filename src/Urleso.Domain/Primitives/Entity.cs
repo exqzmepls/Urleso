b743230f4d68fbadc746d24e9d919a1ccec8d360
exqzmepls <exqzmepls@yandex.ru>
2023-10-01 16:58:22 +0500

    Add primitives

---
+++ src/Urleso.Domain/Primitives/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace Urleso.Domain.Primitives;
+
+public abstract class Entity(EntityId id) : IEquatable<Entity>
+{
+    public EntityId Id { get; } = id;
+
+    public static bool operator ==(Entity? first, Entity? second) =>
+        first?.Equals(second) ?? ReferenceEquals(first, second);
+
+    public static bool operator !=(Entity? first, Entity? second) =>
+        !(first == second);
+
+    public override bool Equals(object? obj) => obj is Entity entity && Equals(entity);
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return other.Id == Id;
+    }
+}
---
+++ src/Urleso.Domain/Primitives/EntityId.cs
@@ -0,0 +1,26 @@
+﻿namespace Urleso.Domain.Primitives;
+
+public readonly struct EntityId : IEquatable<EntityId>
+{
+    private readonly Guid _value;
+
+    private EntityId(Guid value)
+    {
+        _value = value;
+    }
+
+    public static EntityId New()
+    {
+        var value = Guid.NewGuid();
+        return new EntityId(value);
+    }
+
+    public static bool operator ==(EntityId first, EntityId second) => first.Equals(second);
+    public static bool operator !=(EntityId first, EntityId second) => !(first == second);
+
+    public override string ToString() => _value.ToString();
+    public override int GetHashCode() => _value.GetHashCode();
+    public override bool Equals(object? obj) => obj is EntityId other && Equals(other);
+
+    public bool Equals(EntityId other) => _value.Equals(other._value);
+}
