36cc3d1f21da2ea865cfb41060305bd05583b3e1
exqzmepls <exqzmepls@yandex.ru>
2023-10-07 17:02:20 +0500

    Update primitives

--- src/Urleso.Domain/Primitives/Entity.cs
+++ src/Urleso.Domain/Primitives/Entity.cs
@@ -1,20 +1,21 @@
 ﻿namespace Urleso.Domain.Primitives;
 
-public abstract class Entity(EntityId id) : IEquatable<Entity>
+public abstract class Entity<TEntityId>(TEntityId id) : IEquatable<Entity<TEntityId>>
+    where TEntityId : EntityId
 {
-    public EntityId Id { get; } = id;
+    public TEntityId Id { get; } = id;
 
-    public static bool operator ==(Entity? first, Entity? second) =>
+    public static bool operator ==(Entity<TEntityId>? first, Entity<TEntityId>? second) =>
         first?.Equals(second) ?? ReferenceEquals(first, second);
 
-    public static bool operator !=(Entity? first, Entity? second) =>
+    public static bool operator !=(Entity<TEntityId>? first, Entity<TEntityId>? second) =>
         !(first == second);
 
-    public override bool Equals(object? obj) => obj is Entity entity && Equals(entity);
+    public override bool Equals(object? obj) => obj is Entity<TEntityId> entity && Equals(entity);
 
     public override int GetHashCode() => Id.GetHashCode();
 
-    public bool Equals(Entity? other)
+    public bool Equals(Entity<TEntityId>? other)
     {
         if (other is null)
         {
