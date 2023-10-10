ee595ae9612a725797f7c6bd0c3c783f042478df
exqzmepls <exqzmepls@yandex.ru>
2023-10-10 22:15:35 +0500

    Add long url unit tests

---
+++ tests/Urleso.Domain.UnitTests/ShortenedUrls/LongUrlValueObject/LongUrlCreateNegativeTestData.cs
@@ -0,0 +1,31 @@
+﻿using Urleso.Domain.Results;
+using Urleso.Domain.ShortenedUrls;
+using Xunit;
+
+namespace Urleso.Domain.UnitTests.ShortenedUrls.LongUrlValueObject;
+
+internal sealed class LongUrlCreateNegativeTestData : TheoryData<string, Error>
+{
+    public LongUrlCreateNegativeTestData()
+    {
+        WhenEmptyString();
+        WhenNotAbsolute();
+        WhenTooLong();
+    }
+
+    private void WhenEmptyString()
+    {
+        Add(string.Empty, Errors.LongUrl.NonAbsoluteLink);
+    }
+
+    private void WhenNotAbsolute()
+    {
+        Add("not_an_absolute_url", Errors.LongUrl.NonAbsoluteLink);
+    }
+
+    private void WhenTooLong()
+    {
+        var tooLongUrl = "https://example.com/" + new string('a', 1005);
+        Add(tooLongUrl, Errors.LongUrl.LengthTooLong);
+    }
+}
