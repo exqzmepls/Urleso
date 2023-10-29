843bd14158e817ec21e86bf71a8d82017cfcd3a8
exqzmepls <exqzmepls@yandex.ru>
2023-10-29 21:31:36 +0500

    Add Get Long Url By Code Query & Handler

---
+++ src/Urleso.Application/ShortenedUrls/GetLongUrlByCode/GetLongUrlByCodeQuery.cs
@@ -0,0 +1,9 @@
+﻿using Urleso.Application.Abstractions.Messaging;
+using Urleso.Domain.ShortenedUrls;
+
+namespace Urleso.Application.ShortenedUrls.GetLongUrlByCode;
+
+public sealed class GetLongUrlByCodeQuery : IQuery<LongUrl?>
+{
+    public required string Code { get; init; }
+}
---
+++ src/Urleso.Application/ShortenedUrls/GetLongUrlByCode/GetLongUrlByCodeQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Urleso.Application.Abstractions.Data.Repositories;
+using Urleso.Application.Abstractions.Messaging;
+using Urleso.Domain.Results;
+using Urleso.Domain.ShortenedUrls;
+
+namespace Urleso.Application.ShortenedUrls.GetLongUrlByCode;
+
+internal sealed class GetLongUrlByCodeQueryHandler(
+        IShortenedUrlRepository shortenedUrlRepository
+    )
+    : IQueryHandler<GetLongUrlByCodeQuery, LongUrl?>
+{
+    public async Task<TypedResult<LongUrl?>> Handle(GetLongUrlByCodeQuery query, CancellationToken cancellationToken)
+    {
+        var shortenedUrlResult = await shortenedUrlRepository.GetByCodeAsync(query.Code, cancellationToken);
+        if (!shortenedUrlResult.IsSuccess)
+        {
+            return TypedResult<LongUrl?>.Failure(shortenedUrlResult.Error);
+        }
+
+        var shortenedUrl = shortenedUrlResult.Value;
+        return TypedResult<LongUrl?>.Success(shortenedUrl?.LongUrl);
+    }
+}
