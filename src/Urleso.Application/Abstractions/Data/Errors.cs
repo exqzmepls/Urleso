31277e46298c3c62d234b212f976a70e4a4de27e
exqzmepls <exqzmepls@yandex.ru>
2023-11-11 23:34:21 +0500

    Add data errors

---
+++ src/Urleso.Application/Abstractions/Data/Errors.cs
@@ -0,0 +1,9 @@
+﻿using Urleso.Domain.Results;
+
+namespace Urleso.Application.Abstractions.Data;
+
+public static class Errors
+{
+    public static readonly Error DbUpdateError =
+        new("Db.Update", "An error is encountered while saving to the database.");
+}
