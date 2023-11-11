16a8bfb9ac29c4b0216a06fa1932ae95bd0c710e
exqzmepls <exqzmepls@yandex.ru>
2023-11-11 23:38:44 +0500

    Add ShortenedUrlRepository

---
+++ src/Urleso.Infrastructure/Data/Repositories/ShortenedUrlRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Urleso.Application.Abstractions.Data.Repositories;
+using Urleso.Domain.Results;
+using Urleso.Domain.ShortenedUrls;
+using Urleso.Persistence;
+
+namespace Urleso.Infrastructure.Data.Repositories;
+
+internal sealed class ShortenedUrlRepository(
+        ApplicationDbContext dbContext
+    )
+    : IShortenedUrlRepository
+{
+    private DbSet<ShortenedUrl> ShortenedUrls => dbContext.Set<ShortenedUrl>();
+
+    public async Task<TypedResult<bool>> IsCodeExistAsync(UrlCode code, CancellationToken cancellationToken)
+    {
+        var isCodeExist = await ShortenedUrls.AnyAsync(u => u.Code == code, cancellationToken);
+        return TypedResult<bool>.Success(isCodeExist);
+    }
+
+    public async Task<Result> AddAsync(ShortenedUrl url, CancellationToken cancellationToken)
+    {
+        await ShortenedUrls.AddAsync(url, cancellationToken);
+        return Result.Success();
+    }
+
+    public async Task<TypedResult<ShortenedUrl?>> GetByCodeOrDefaultAsync(string code,
+        CancellationToken cancellationToken)
+    {
+        var shortenedUrl = await ShortenedUrls.SingleOrDefaultAsync(u => u.Code.Value == code, cancellationToken);
+        return TypedResult<ShortenedUrl?>.Success(shortenedUrl);
+    }
+}
