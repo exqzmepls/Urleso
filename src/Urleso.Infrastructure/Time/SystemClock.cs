aaf49071328d608f2f79610303adb5ac918de2ed
exqzmepls <exqzmepls@yandex.ru>
2023-11-11 23:38:24 +0500

    Add SystemClock

---
+++ src/Urleso.Infrastructure/Time/SystemClock.cs
@@ -0,0 +1,8 @@
+﻿using Urleso.Application.Abstractions.Time;
+
+namespace Urleso.Infrastructure.Time;
+
+internal sealed class SystemClock : IClock
+{
+    public DateTime GetUtcNow() => DateTime.UtcNow;
+}
