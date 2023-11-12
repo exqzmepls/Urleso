05679fb52beeec397c1a49f61cb17168a876ca68
exqzmepls <exqzmepls@yandex.ru>
2023-11-12 22:25:36 +0500

    Add shortened url endpoints

---
+++ src/Urleso.Presentation/Api/ShortenedUrls/CreateShortenedUrl/ShortenedUrlDetails.cs
@@ -0,0 +1,6 @@
+﻿namespace Urleso.Presentation.Api.ShortenedUrls.CreateShortenedUrl;
+
+public sealed class ShortenedUrlDetails
+{
+    public required string UrlCode { get; init; }
+}
---
+++ src/Urleso.Presentation/Api/ShortenedUrls/ShortenedUrlEndpoints.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Urleso.Application.Abstractions.Messaging;
+using Urleso.Presentation.Api.ShortenedUrls.CreateShortenedUrl;
+using Urleso.Presentation.Shared;
+
+namespace Urleso.Presentation.Api.ShortenedUrls;
+
+public sealed class ShortenedUrlEndpoints : BaseModule
+{
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var shortenedUrlsGroup = app.MapGroup("shortened-urls");
+
+        shortenedUrlsGroup.MapPost("", CreateShortenedUrlAsync);
+    }
+
+    private static async Task<Results<Ok<ShortenedUrlDetails>, BadRequest<ErrorDetails>>> CreateShortenedUrlAsync(
+        [AsParameters] CreateShortenedUrlRequest request,
+        [FromServices] ISender sender,
+        CancellationToken cancellationToken
+    )
+    {
+        var command = request.MapToCreateShortenedUrlCommand();
+        var commandResult = await sender.SendAsync(command, cancellationToken);
+        if (!commandResult.IsSuccess)
+        {
+            var errorDetails = commandResult.Error.MapToErrorDetails();
+            return TypedResults.BadRequest(errorDetails);
+        }
+
+        var shortenedUrlDetails = commandResult.Value.MapToShortenedUrlDetails();
+        return TypedResults.Ok(shortenedUrlDetails);
+    }
+}
