7174f0c81a682bcaa0473704aba0e7ae38697e89
exqzmepls <exqzmepls@yandex.ru>
2024-01-31 20:29:57 +0500

    Add web app from template

---
+++ src/Urleso.Web/Program.cs
@@ -0,0 +1,24 @@
+using Urleso.Web.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
