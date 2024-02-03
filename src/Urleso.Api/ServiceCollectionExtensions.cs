f7091ba52818b46edb1c9518bb3e0e8afe55bfbe
exqzmepls <exqzmepls@yandex.ru>
2024-02-04 00:14:06 +0500

    Add nswag client generation

---
+++ src/Urleso.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Urleso.Api;
+
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddOpenApiGen(this IServiceCollection services)
+    {
+        services.AddOpenApiDocument(settings =>
+        {
+            var assembly = typeof(Program).Assembly;
+            var assemblyName = assembly.GetName();
+            settings.Title = assemblyName.Name;
+        });
+
+        return services;
+    }
+}
