68cce906b1b828aa036ae380b5e834f2e59e916a
exqzmepls <exqzmepls@yandex.ru>
2024-02-10 20:46:48 +0500

    Add redirect settings

---
+++ src/Urleso.Presentation/Api/ShortenedUrls/CreateShortenedUrl/RedirectSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Urleso.Presentation.Api.ShortenedUrls.CreateShortenedUrl;
+
+internal sealed class RedirectSettings
+{
+    public const string ConfigurationSection = "Redirect";
+
+    [Required]
+    [Url]
+    public required string BaseAddress { get; init; } // must have "/" in the end
+}
--- src/Urleso.Presentation/DependencyInjection.cs
+++ src/Urleso.Presentation/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Carter;
 using Microsoft.Extensions.DependencyInjection;
+using Urleso.Presentation.Api.ShortenedUrls.CreateShortenedUrl;
 
 namespace Urleso.Presentation;
 
@@ -7,6 +8,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.AddOptions<RedirectSettings>()
+            .BindConfiguration(RedirectSettings.ConfigurationSection)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
         var presentationAssembly = typeof(DependencyInjection).Assembly;
         var assemblyCatalog = new DependencyContextAssemblyCatalog(new[]
         {
